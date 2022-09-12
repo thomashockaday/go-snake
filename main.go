0e2d8a63674aed07d7a24340cd115e6fdd92fa11
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2022-09-12 16:35:28 +0100

    Add food

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
 
 // key
 // Empty square: 0
 // Snake head:   2
+// Food:         5
 
 type Game struct {
 	Rows int
 	Columns int
 	Board [][]int
 	SnakeHead [2]int
+	FoodPosition [2]int
 }
 
 func main() {
@@ -22,6 +30,8 @@ func main() {
 	game.Board = game.setupBoard();
 	game.SnakeHead[0] = 0
 	game.SnakeHead[1] = 0
+
+	game.placeFood()
 
 	game.printBoard()
 }
@@ -47,4 +57,29 @@ func (game Game) printBoard() {
 		fmt.Println()
 	}
 	fmt.Printf("Coordinate of snake head: {%d, %d}\n", game.SnakeHead[0], game.SnakeHead[1])
+}
+
+func (game Game) placeFood() {
+	possibleXY := []string{}
+
+	for x := range game.Board {
+		for y := range game.Board[x] {
+			if (game.Board[x][y] == 0) {
+				possibleXY = append(possibleXY, fmt.Sprintf("%d, %d", x, y))
+			}
+		}
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	lowerBoundary := 0
+	higherBoundary := len(possibleXY) - 1
+	randomPosition := lowerBoundary + rand.Intn(higherBoundary - lowerBoundary + 1)
+
+	value := strings.Split(possibleXY[randomPosition], ", ")
+	pX, _ := strconv.Atoi(value[0])
+	pY, _ := strconv.Atoi(value[1])
+
+	game.FoodPosition[0] = pX
+	game.FoodPosition[1] = pY
+	game.Board[pX][pY] = 5
 }
